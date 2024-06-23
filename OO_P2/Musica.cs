e848f340d107b6bfd81ec47c859a6289d8820692
vinicius <vinicastro881@gmail.com>
2024-06-23 17:32:17 -0300

    Organização das pastas a partir dos módulos dos cursos da Alura

---
+++ OO_P2/Musica.cs
@@ -0,0 +1,21 @@
+﻿class Musica
+{
+    public string nome, artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if( disponivel)
+        {
+            Console.WriteLine($"Disponível no seu plano");
+        }
+        else
+        {
+            Console.WriteLine($"Não disponível no seu plano");
+        }
+    }
+}
---
+++ OO_P2/Program.cs
@@ -0,0 +1,14 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Earthquake";
+musica1.artista = "Tyler";
+musica1.duracao = 300;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+musica2.duracao = 280;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
